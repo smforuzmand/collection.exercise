a19ac2c55d84e4b909721d4ef1202094b574c00b
Mohsen Forouzmand <94297998+smforuzmand@users.noreply.github.com>
2022-07-14 12:38:15 +0200

    Collection Exercise

--- src/main/java/lexicon/se/ExerciseCollection.java
+++ src/main/java/lexicon/se/ExerciseCollection.java
@@ -1,8 +1,6 @@
 package lexicon.se;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ExerciseCollection {
 
@@ -31,10 +29,19 @@ public class ExerciseCollection {
         weekDays.add(4, "Friday");
         weekDays.add(5, "Saturday");
         weekDays.add(6, "Sunday");
+        //Collections.sort(weekDays);   //Just To Test How Does It Work?
 
         for (String day : weekDays) {
             System.out.println(day);
         }
+        Iterator iter = weekDays.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
+
+        //weekDays.contains("Saturday");
+        System.out.println(weekDays.contains("Saturday"));
     }
 
     //3. Create a new list and populate it with the days of the week excluding THURSDAY. Lastly,
@@ -55,10 +62,11 @@ public class ExerciseCollection {
         System.out.println(weekDays);
 
     }
+
     //4. Create a new list and populate it with the days of the week. Then create a new list out of the
     //first three elements of the first list using a subList.
-    public static void ex4(){
-        ArrayList<String> weekDays=new ArrayList<>();
+    public static void ex4() {
+        ArrayList<String> weekDays = new ArrayList<>();
         weekDays.add(0, "Monday");
         weekDays.add(1, "Tuesday");
         weekDays.add(2, "Wednesday");
@@ -67,24 +75,119 @@ public class ExerciseCollection {
         weekDays.add(5, "Saturday");
         weekDays.add(6, "Sunday");
 
-        List<String>sublist=weekDays.subList(3,7);
+        List<String> sublist = weekDays.subList(3, 7);
         System.out.println("sub list of weekdays = " + sublist);
-
-public static void ex5(){
-
-        }
-
-
-
-
-
 
 
     }
 
+    public static void ex5() {
+        //5. Create a new hashset and populate it with the days of the week. Lastly, print the set out and
+        //pay attention to the order of the elements.
+        HashSet<String> weekdays = new HashSet<>();
+        weekdays.add("MOnday");
+        weekdays.add("Tuesday");
+        weekdays.add("Wednesday");
+        weekdays.add("Thursday");
+        weekdays.add("Friday");
+        weekdays.add("Saturday");
+        weekdays.add("Sunday");
+        Iterator iter = weekdays.iterator();
+        while (iter.hasNext()) {//no order set followed by method
+            System.out.println(iter.next());
+        }
 
+    }
+
+    public static void ex6() {
+        //6. Create a new hashSet and populate it with the days of the week. Then convert the hashSet to
+        //an arrayList.
+        HashSet<String> weekdays = new HashSet<>();
+        weekdays.add("MOnday");
+        weekdays.add("Tuesday");
+        weekdays.add("Wednesday");
+        weekdays.add("Thursday");
+        weekdays.add("Friday");
+        weekdays.add("Saturday");
+        weekdays.add("Sunday");
+        System.out.println("the HashSet weekdays contains :" + weekdays);
+        //Convert to  Array
+        String[] array = weekdays.toArray(new String[7]);//#1
+        System.out.println("array = " + weekdays);
+        String[] array2 = new String[weekdays.size()];//#2 Having a fixed size array , extract the weekday size and assign to array2
+        weekdays.toArray(array2);//method to change the list to array
+        //Displaying the Array;
+
+        for (String str : array2) {
+            System.out.println(str);
+        }
+        //Convert to ArrayList
+        ArrayList<String> arrayList = new ArrayList<String>(weekdays);
+        System.out.println("arrayList = " + arrayList);
+
+    }
+
+    public static void ex7() {
+        //7. Create a new hashSet and populate it with random names. Then convert the hashSet to an
+        //arrayList. Lastly, manually sort the list in alphabetical order and print it out.
+
+        HashSet<String> randomNames = new HashSet<String>();
+        randomNames.add("Erik");
+        randomNames.add("Eva");
+        randomNames.add("Martin");
+        randomNames.add("Johnnas");
+        randomNames.add("Johakim");
+        randomNames.add("Sara");
+        List<String> newRandomName = new ArrayList<String>(randomNames);
+        System.out.println("unsorted names as follow  :");
+        for (String names : newRandomName) {
+            System.out.println(names);//print out unsorted list!
+        }
+        System.out.println("sourted names after using Collection.sort");
+        Collections.sort(newRandomName);
+
+        for (String sortedNames : newRandomName) {
+            System.out.println(sortedNames);
+        }
+    }
+
+    public static void ex8() {
+        //8. Create a new hashSet and populate it with random names. Lasty, sort the names in
+        // alphabetical order using only a Set or a child of Set.
+        HashSet<String> randomNames = new HashSet<String>();
+        randomNames.add("Erik");
+        randomNames.add("Eva");
+        randomNames.add("Martin");
+        randomNames.add("Johnnas");
+        randomNames.add("Johakim");
+        randomNames.add("Sara");
+        System.out.println("what we have before sorting :");
+        for (String names : randomNames) {
+            System.out.println(names);
+        }
+        //Sorting with treeset
+        TreeSet<String> sortedNames = new TreeSet<String>(randomNames);
+        System.out.println("Sorting with Set and TreeSet :");
+        for (String names : sortedNames) {
+            System.out.println(names);
+        }
+        
+    }
+    public static void ex9() {
+        //9. Create a new hashMap of type <Integer,String> and populate it with elements containing an
+        //id(Integer) and a car brand(String). Lastly, print out the entire hashMap.
+
+
+    }
 
 }
+
+
+
+
+
+
+
 
 
 
--- src/main/java/lexicon/se/Main.java
+++ src/main/java/lexicon/se/Main.java
@@ -6,7 +6,11 @@ public class Main {
         //ExerciseCollection.ex1();
         //ExerciseCollection.ex2();
         //ExerciseCollection.ex3();
-        ExerciseCollection.ex4();
+        //ExerciseCollection.ex4();
+        //ExerciseCollection.ex5();
+        //ExerciseCollection.ex6();
+        //ExerciseCollection.ex7();
+        ExerciseCollection.ex8();
     }
 
 }
